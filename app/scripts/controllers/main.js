48d4ae5b7d1a42e96a9593c3b96d18f67a27459f
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-05-08 11:40:55 -0400

    Remove youtube faked thumbnails

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,23 +3,24 @@
 angular.module('peepoltvApp')
   .controller('MainCtrl', ['$scope', 'Streams', function ($scope, Streams) {
 
-    // Temporary thubnails
-    var tmpThumbs = ['fopr63yr3ps', 'VgPBse5BkA8', 'NR7JUM23WN8'];
-
     // Create map
     var map = L.mapbox.map('map', 'peepoltv.map-ujvx87td');
 
     // Get the streams basen on geolocation
     Streams.search({}, function(r){
-        $scope.streams = _.map(r, function(s, k){
-          s.thumb = tmpThumbs[k];
+        $scope.streams = _.map(r, function(s){
           s.properties['marker-size'] = 'medium';
           s.properties['marker-color'] = '#aa56ff';
+          s.properties['marker-symbol'] = 'cinema';
           return s;
         });
 
         // Create and add marker layer
-        var markerLayer = L.mapbox.markerLayer($scope.streams);
+        var markerLayer = L.mapbox.markerLayer();
+        markerLayer.setGeoJSON({
+          type: 'FeatureCollection',
+          features: $scope.streams
+        });
         markerLayer.addTo(map);
 
         markerLayer.eachLayer(function(marker) {
@@ -27,7 +28,6 @@ angular.module('peepoltvApp')
 
           // Create custom popup content
           var popupContent = '<a target="_blank" href="' + feature.url + '">' +
-                              '<img src="http://img.youtube.com/vi/' + feature.thumb + '/1.jpg" />' +
                               '<h3>' + feature.title + '</h3>' +
                              '</a>';
 
@@ -43,4 +43,3 @@ angular.module('peepoltvApp')
         map.fitBounds(markerLayer.getBounds());
       });
   }]);
-          s.properties['marker-symbol'] = 'cinema';
