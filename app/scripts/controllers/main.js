78518260820730fd6432e789679554745271b40a
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-05-09 17:56:36 -0400

    Use the thumbnails from the api

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,16 +3,12 @@
 angular.module('peepoltvApp')
   .controller('MainCtrl', ['$scope', 'Streams', function ($scope, Streams) {
 
-    // Temporary thubnails
-    var tmpThumbs = ['fopr63yr3ps', 'VgPBse5BkA8', 'NR7JUM23WN8'];
-
     // Create map
     var map = L.mapbox.map('map', 'peepoltv.map-ujvx87td');
 
     // Get the streams basen on geolocation
     Streams.search({}, function(r){
-        $scope.streams = _.map(r, function(s, k){
-          s.thumb = tmpThumbs[k];
+        $scope.streams = _.map(r, function(s){
           s.properties['marker-size'] = 'medium';
           s.properties['marker-color'] = '#aa56ff';
           return s;
@@ -27,7 +23,7 @@ angular.module('peepoltvApp')
 
           // Create custom popup content
           var popupContent = '<a target="_blank" href="' + feature.url + '">' +
-                              '<img src="http://img.youtube.com/vi/' + feature.thumb + '/1.jpg" />' +
+                              '<img src="' + feature.thumbs.small + '" />' +
                               '<h3>' + feature.title + '</h3>' +
                              '</a>';
 
