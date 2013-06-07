855cbe616cc50b2da5c24659de6f65be21f8bbbc
Chris Rogers <chris@peepol.tv>
2013-06-07 11:42:56 -0400

    edits

--- app/scripts/controllers/channels.js
+++ app/scripts/controllers/channels.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('peepoltvApp')
-  .controller('ChannelsCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ChannelsCtrl', ['$scope', 'Streams', function ($scope, Streams) {
+    
+    // Get the streams based on geolocation
+    Streams.search({}, function(r){
+        $scope.streams = _.map(r, function(s){
+          s.properties['marker-size'] = 'medium';
+          s.properties['marker-color'] = '#aa56ff';
+          s.properties['marker-symbol'] = 'cinema';
+          return s;
+    });
+    
   }]);
