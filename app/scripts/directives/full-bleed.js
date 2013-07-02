825693fc402bfc3cef095cc5cb56a4993b126b14
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-07-02 11:02:13 -0400

    Make golive video full screen, close #23

---
+++ app/scripts/directives/full-bleed.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('peepoltvApp')
+  .directive('fullBleed', function ($window) {
+    return function(scope, element){
+      var w = angular.element($window);
+      var e = angular.element(element);
+
+      // Make the element move backwards
+      e.css({
+        'z-index': -10,
+        position: 'relative'
+      });
+
+      scope.getValues = function () {
+        return {
+          'height': w.height(),
+          'width': w.width(),
+          'top': e.position().top,
+          'left': e.position().left
+        };
+      };
+      scope.$watch(scope.getValues, function (newValue, oldValue) {
+        // resize Grid to optimize height and position
+        $(element)
+          .width(newValue.width)
+          .height(newValue.height)
+          .css({
+            'margin-left': -newValue.left,
+            'margin-top': -newValue.top
+          });
+      }, true);
+
+      w.bind('resize', function () {
+        scope.$apply();
+      });
+    }
+  });
---
+++ test/spec/directives/full-bleed.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Directive: resize', function () {
+  beforeEach(module('peepoltvApp'));
+
+  var element;
+
+  it('should make hidden element visible', inject(function ($rootScope, $compile) {
+    element = angular.element('<resize></resize>');
+    element = $compile(element)($rootScope);
+    expect(element.text()).toBe('this is the resize directive');
+  }));
+});
