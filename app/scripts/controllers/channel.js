0db627ffd4ebb68c327d412072b09ca704c1755a
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-07-07 18:59:11 -0400

    Make the channels carrouse work

--- app/scripts/controllers/channel.js
+++ app/scripts/controllers/channel.js
@@ -4,5 +4,13 @@ angular.module('peepoltvApp')
   .controller('ChannelCtrl', function ($scope, channelService, $routeParams) {
 
     // Get the channel
-    $scope.channel = channelService.resource.get({channelId: $routeParams.channelName});
+    $scope.channel = channelService.resource.get({channelId: $routeParams.channelName}, function(){
+      // The playing stream
+      $scope.mainStream = $scope.channel.streams[0];
+    });
+
+    $scope.changeMainStream = function(stream){
+      $scope.mainStream = stream;
+    }
+
   });
--- app/scripts/controllers/channels/local.js
+++ app/scripts/controllers/channels/local.js
@@ -4,5 +4,14 @@ angular.module('peepoltvApp')
   .controller('ChannelsLocalCtrl', function ($scope, channelService) {
 
     // Get the streams based on geolocation
-    $scope.channels = channelService.resource.search();
+    $scope.channels = channelService.resource.search({}, function(){
+
+      $scope.channel = $scope.channels[2];
+      $scope.mainStream = $scope.channel.streams[0];
+
+    });
+
+    $scope.changeMainStream = function(stream){
+      $scope.mainStream = stream;
+    }
   });
