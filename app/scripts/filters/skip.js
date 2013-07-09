f51a268078b5af2f31a5b75829bde7788e7d1323
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-07-08 22:10:10 -0400

    Fix bug in the skip filter

--- app/scripts/filters/skip.js
+++ app/scripts/filters/skip.js
@@ -3,6 +3,11 @@
 angular.module('peepoltvApp')
   .filter('skip', function () {
     return function (input, count) {
-      return _.rest(input, count);
+      if(angular.isArray(input)){
+        return _.rest(input, count);
+      }
+      else{
+        return input;
+      }
     };
   });
