a2257e8efff7e04de708dbf4557aa0d2df6212e9
Emilio Blanco <emilioeduardob@gmail.com>
2013-09-24 14:39:24 -0400

    Adds gravatar filter

---
+++ app/scripts/filters/gravatar.js
@@ -0,0 +1,13 @@
+'user strict'
+
+angular.module('peepoltvApp')
+  .filter('gravatar', function() {
+    return function(input, size, defaultImage) {
+      if (input) {
+        var emailHash = SparkMD5.hash(input.toLowerCase().trim());
+        return "http://www.gravatar.com/avatar/" + emailHash + "?s=" + size;
+      } else {
+        return defaultImage;
+      }
+    }
+  });
