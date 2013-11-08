a773f4e5a9d3a213b013507450bff2d1a3ea929e
Emilio Blanco <emilioeduardob@gmail.com>
2013-11-08 09:44:54 -0300

    Changes validation logic to user setValidity

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -18,14 +18,17 @@ angular.module('peepoltvApp')
         authService.user.name = user.name;
       },
       function(e){
-        if(angular.isObject(e.data) && e.data.info.email.length){
-          if(e.data.info.email[0] === 'has already been taken'){
-            // The email is already taken
-            $scope.alert = 'email';
-          }
-          else{
-            $scope.alert = 'other';
-          }
+        if(angular.isObject(e.data) && e.data.info){
+          var info = e.data.info;
+
+          //mark fields as invalid if there's a need
+          $scope.signupForm.email.$setValidity("taken", (info.email && info.email[0].match(/taken/) == null));
+          $scope.signupForm.password.$setValidity("minlength", (info.password && info.password[0].match(/short/) == null));
+          $scope.signupForm.username.$setValidity("taken", (info.username && info.username[0].match(/taken/) == null));
+
+          // flag for showing alerts(should be removed once we have inline error messages)
+          $scope.alert = true;
+
         }
         else{
           $scope.alert = 'other';
