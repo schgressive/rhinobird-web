3b1e2eb01689a2d3044fc7ded29d52b0f4fc13aa
Chris Rogers <chris@peepol.tv>
2013-11-14 16:07:37 -0500

    testing Owl Carousel
    adding the stuff it needs… [#60533710]

---
+++ app/scripts/main.js
@@ -0,0 +1,27 @@
+/* initialize tooltips */
+	if (!window.matchMedia || (window.matchMedia("(min-width: 767px)").matches))
+	$(function () {
+		$('.base').tooltip({
+	      selector: '[rel="tooltip"]'
+	    })
+	  });
+
+$(document).ready(function() {
+
+	/* bootstrap tooltips */
+	$('.base').tooltip({
+	   selector: '[rel="tooltip"]'
+	 });
+
+ 	/* video slider */
+  $("#owl-example").owlCarousel();
+
+  /* collapses menu in mobile after clicking link */
+  $('#hideonclick a').click(function (e) {
+	        e.preventDefault();
+	        $(this).tab('show');
+	        if ($('.btn').is(":visible"))
+	            $('.btn').click();
+	    });
+
+});
