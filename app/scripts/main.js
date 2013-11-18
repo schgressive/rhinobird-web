17808b9d335fd6fe24b899dc77f8cbcd62044963
Chris Rogers <chris@peepol.tv>
2013-11-18 12:55:56 -0500

    edits to carousel
    WIP

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	 });
 
  	/* video slider */
-   $("#streamSet").owlCarousel();
+ 	$('.owl-carousel').owlCarousel();
 
   /* collapses menu in mobile after clicking link */
   $('#hideonclick a').click(function (e) {
