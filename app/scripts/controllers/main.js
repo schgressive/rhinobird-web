342c7cd71a2daa9729a101d4bab5395a6ad1a634
Chris Rogers <chris@peepol.tv>
2013-11-18 15:38:27 -0500

    Owl Carousel WIP
    testing out features [#60533710]

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 angular.module('peepoltvApp')
 
-  .controller('MainCtrl', function ($scope, streamService, $location, authService) {
+  .controller('MainCtrl', function ($scope, streamService, $location, authService, $timeout) {
 
     // Get the streams based on geolocation
     $scope.streams = streamService.resource.search({});
@@ -14,9 +14,32 @@ angular.module('peepoltvApp')
 
     $scope.user = authService.user;
 
-    $scope.$watch('streams', function(){
+    $scope.$watchCollection('streams', function(a, b){
+			if (a.length == 0)
+	    	return;
       /* video slider */
-      $('.owl-carousel').owlCarousel();
+      console.log("will wait")
+			$timeout(function(){
+
+	      $('.owl-carousel').owlCarousel({
+		      	items: 6,
+		      	itemsDesktop : [1199,4],
+						itemsDesktopSmall : [980,4],
+						itemsTablet: [768,3],
+						itemsMobile : [479,3],
+		      	pagination: false,
+		      	navigation: true
+	      });
+	      $(".next").click(function(){
+						owl.trigger('owl.next');
+				})
+				$(".prev").click(function(){
+						owl.trigger('owl.prev');
+				});
+				console.log("ok")
+				//debugger;
+
+			},500);
     });
 
   });
