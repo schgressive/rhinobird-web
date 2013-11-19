2ab585562c8916a0adc16a33fb7a8ce81660ae04
Chris Rogers <chris@peepol.tv>
2013-11-19 10:31:33 -0500

    Place Owl Carousel in Channel and Stream template
    Carousel works in Channel and Stream templates.
    Should be revised at a later time. [#60533710]

--- app/scripts/controllers/channel.js
+++ app/scripts/controllers/channel.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('peepoltvApp')
-  .controller('ChannelCtrl', function ($scope, channelService, $routeParams, AuthService) {
+  .controller('ChannelCtrl', function ($scope, channelService, $routeParams, AuthService, $timeout) {
 
 		$scope.user = AuthService.user;
 
@@ -14,5 +14,33 @@ angular.module('peepoltvApp')
     $scope.changeMainStream = function(stream){
       $scope.mainStream = stream;
     }
+
+		$scope.$watchCollection('channel.streams', function(a, b){
+			if (a.length == 0)
+	    	return;
+      /* video slider */
+      console.log("will wait")
+			$timeout(function(){
+
+	      $('.owl-carousel').owlCarousel({
+		      	items: 6,
+		      	itemsDesktop : [1199,4],
+						itemsDesktopSmall : [980,4],
+						itemsTablet: [768,3],
+						itemsMobile : [479,3],
+		      	pagination: false,
+		      	navigation: true
+	      });
+	      $(".next").click(function(){
+						owl.trigger('owl.next');
+				})
+				$(".prev").click(function(){
+						owl.trigger('owl.prev');
+				});
+				console.log("ok")
+				//debugger;
+
+			},500);
+    });
 
   });
--- app/scripts/controllers/stream.js
+++ app/scripts/controllers/stream.js
@@ -1,10 +1,42 @@
 'use strict';
 
 angular.module('peepoltvApp')
-  .controller('StreamCtrl', function ($scope, streamService, $routeParams, AuthService) {
+  .controller('StreamCtrl', function ($scope, streamService, $routeParams, AuthService, $timeout) {
 
 		$scope.user = AuthService.user;
 
     // Get the stream
     $scope.stream = streamService.resource.get({streamId: $routeParams.streamId});
-  });
+
+		// Get the streams based on geolocation
+    $scope.streams = streamService.resource.search({});
+
+		$scope.$watchCollection('streams', function(a, b){
+			if (a.length == 0)
+	    	return;
+      /* video slider */
+      console.log("will wait")
+			$timeout(function(){
+
+	      $('.owl-carousel').owlCarousel({
+		      	items: 6,
+		      	itemsDesktop : [1199,4],
+						itemsDesktopSmall : [980,4],
+						itemsTablet: [768,3],
+						itemsMobile : [479,3],
+		      	pagination: false,
+		      	navigation: true
+	      });
+	      $(".next").click(function(){
+						owl.trigger('owl.next');
+				})
+				$(".prev").click(function(){
+						owl.trigger('owl.prev');
+				});
+				console.log("ok")
+				//debugger;
+
+			},500);
+    });
+
+});
