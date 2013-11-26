29b62c6a255447579dac8359a056aa09925afc9e
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-11-26 16:12:36 -0300

    Adds a directive to use the owl carousel, [finish #61043636]

--- app/scripts/controllers/channel.js
+++ app/scripts/controllers/channel.js
@@ -15,32 +15,4 @@ angular.module('peepoltvApp')
       $scope.mainStream = stream;
     }
 
-		$scope.$watchCollection('channel.streams', function(a, b){
-			if (a && a.length == 0)
-	    	return;
-      /* video slider */
-      console.log("will wait")
-			$timeout(function(){
-
-	      $('.owl-carousel').owlCarousel({
-		      	items: 6,
-		      	itemsDesktop : [1199,4],
-						itemsDesktopSmall : [980,4],
-						itemsTablet: [768,3],
-						itemsMobile : [479,3],
-		      	pagination: false,
-		      	navigation: true
-	      });
-	      $(".next").click(function(){
-						owl.trigger('owl.next');
-				})
-				$(".prev").click(function(){
-						owl.trigger('owl.prev');
-				});
-				console.log("ok")
-				//debugger;
-
-			},500);
-    });
-
   });
--- app/scripts/controllers/stream.js
+++ app/scripts/controllers/stream.js
@@ -11,32 +11,4 @@ angular.module('peepoltvApp')
 		// Get the streams based on geolocation
     $scope.streams = Stream.$search();
 
-		$scope.$watchCollection('streams', function(a, b){
-			if (a.length == 0)
-	    	return;
-      /* video slider */
-      console.log("will wait")
-			$timeout(function(){
-
-	      $('.owl-carousel').owlCarousel({
-		      	items: 6,
-		      	itemsDesktop : [1199,4],
-						itemsDesktopSmall : [980,4],
-						itemsTablet: [768,3],
-						itemsMobile : [479,3],
-		      	pagination: false,
-		      	navigation: true
-	      });
-	      $(".next").click(function(){
-						owl.trigger('owl.next');
-				})
-				$(".prev").click(function(){
-						owl.trigger('owl.prev');
-				});
-				console.log("ok")
-				//debugger;
-
-			},500);
-    });
-
 });
