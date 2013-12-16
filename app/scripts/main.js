5e49686ebae17604bbdab184f526bea2ad7d73e7
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-12-16 15:56:46 -0300

    Fix some flexslider bugs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,36 +1,39 @@
 /* initialize tooltips */
-	if (!window.matchMedia || (window.matchMedia("(min-width: 767px)").matches))
-	$(function () {
-		$('.base').tooltip({
-	      selector: '[rel="tooltip"]'
-	    })
-	  });
+if (!window.matchMedia || (window.matchMedia("(min-width: 767px)").matches)){
+  $(function () {
+    $('.base').tooltip({
+      selector: '[rel="tooltip"]'
+    });
+  });
+}
 
 $(document).ready(function() {
 
-	/* bootstrap tooltips */
-	$('.base').tooltip({
-	   selector: '[rel="tooltip"]'
-	 });
+  /* bootstrap tooltips */
+  $('.base').tooltip({
+   selector: '[rel="tooltip"]'
+ });
 
   /* collapses menu in mobile after clicking link */
   $('#hideonclick a').click(function (e) {
-	        e.preventDefault();
-	        $(this).tab('show');
-	        if ($('.btn').is(":visible"))
-	            $('.btn').click();
-	    });
+    e.preventDefault();
+    $(this).tab('show');
+    if ($('.btn').is(":visible")){
+      $('.btn').click();
+    }
+  });
 
-	/*hookup flexslider*/
-	/*
+  /*hookup flexslider*/
+  /*
 $('.body').fadeIn(800);
-			});
+      });
 */
+});
 
-	$(window).load(function() {
-		$('.flexslider').flexslider({
-			animation: 'slide',
-			directionNav: false,
-			controlNav: false,
-		});
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: 'slide',
+    directionNav: false,
+    controlNav: false
+  });
 });
