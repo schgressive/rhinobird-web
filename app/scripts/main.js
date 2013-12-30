9ecf68b96e2c8730eabbe346d11b88d7ff4a397d
Chris Rogers <chris@peepol.tv>
2013-12-30 10:45:34 -0500

    Added a refresh placeholder for infinite scroll
    [#61070860] @fheusser Infinite scroll will probably be an angular
    function. I added a placeholder based on Hook.js. Inifite scroll should
    be created as a card for @blackjid

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,26 +14,4 @@ $(document).ready(function() {
    selector: '[rel="tooltip"]'
  });
 
-  /* collapses menu in mobile after clicking link */
-  $('#hideonclick a').click(function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-    if ($('.btn').is(":visible")){
-      $('.btn').click();
-    }
-  });
-
-  /*hookup flexslider*/
-  /*
-$('.body').fadeIn(800);
-      });
-*/
-});
-
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: 'slide',
-    directionNav: false,
-    controlNav: false
-  });
 });
