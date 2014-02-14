da4cc1259af7b94f2bb5c70fa60aa5e187fb486e
Emilio Blanco <emilioeduardob@gmail.com>
2014-02-14 09:06:38 -0300

    Hooks share buttons

--- app/scripts/controllers/stream.js
+++ app/scripts/controllers/stream.js
@@ -1,9 +1,11 @@
 'use strict';
 
 angular.module('peepoltv.controllers')
-  .controller('StreamCtrl', function ($scope, $stateParams, $timeout, Stream, AuthService) {
+  .controller('StreamCtrl', function ($scope, $stateParams, $timeout, Stream, AuthService, $location) {
 
 		$scope.user = AuthService.user;
+    $scope.url = encodeURIComponent($location.absUrl());
+    $scope.shareTextEnconded = escape("Share this video!");
 
     // Get the stream
     $scope.stream = Stream.$find($stateParams.streamId);
