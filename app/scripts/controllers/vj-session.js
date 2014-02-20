0eb44b4f92f83ae730077c66f63b89b582dc2a5c
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-02-20 17:43:28 -0300

    [vjsession] Activate current stream when the active stream is changed

--- app/scripts/controllers/vj-session.js
+++ app/scripts/controllers/vj-session.js
@@ -15,7 +15,17 @@ angular.module('peepoltv.controllers')
     $scope.vjstreams = user.vjstreams.$fetch();
 
     // Create a socket data connection with the user token
-    VjService.startListening(user.vjToken);
+    VjService.startListening(user.vjToken).then(function(){
+      VjService.socket.stream.addEventListener('stream-data', function(streamEvent){
+        $scope.$apply(function(){
+          if(streamEvent.msg.event === 'active-stream-change'){
+            var streamToActivate = _.find($scope.vjstreams, function(s){return s.streamId === streamEvent.msg.params.streamId;});
+            streamToActivate.active = true;
+            $scope.currentStream = streamToActivate.stream;
+          }
+        });
+      });
+    });
 
     // Set the active stream as the current stream in the scope
     $scope.$on('licode-video-created', function(event, stream){
