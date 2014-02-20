af9c011f03b9cd73b054de722f31403bedf042e6
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-02-20 16:42:59 -0300

    Refresh the active status from vjstream when a stream gets activated

--- app/scripts/models/vj-stream.js
+++ app/scripts/models/vj-stream.js
@@ -9,7 +9,23 @@ angular.module('peepoltv.models')
       this.setPrimaryKey('streamId');
 
       this.on('after-add', function(vjStream){
-          vjStream.streamId = vjStream.stream.id;
+
+        var collection = this;
+
+        // Add the stream id to the vj stream
+        vjStream.streamId = vjStream.stream.id;
+
+        // Add a hook the each element to mantain the active status
+        // when a vj stream is set as active
+        vjStream.$on('after-save', function(){
+          _.each(collection, function(vjs){
+            if(vjs.active && vjs.streamId !== vjStream.streamId){
+              vjs.active = false;
+            }
+          });
         });
+
+
+      });
     });
   });
