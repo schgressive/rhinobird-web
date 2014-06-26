21dbc01e6e7ed37f0982c2b30f498341621efdf6
Emilio Blanco <emilioeduardob@gmail.com>
2014-06-26 16:13:00 -0400

    Hooks up connect and current state

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -5,6 +5,34 @@ angular.module('rhinobird.controllers')
 
     $scope.self = $scope;
 
+    var openDialog = function(uri, name, options, closeCallback) {
+      var win = window.open(uri, name, options);
+      var interval = window.setInterval(function() {
+        try {
+          if (win == null || win.closed) {
+            window.clearInterval(interval);
+            closeCallback(win);
+          }
+        }
+        catch (e) {
+        }
+      }, 1000);
+      return win;
+    };
+
+
+    $scope.connectTwitter = function() {
+      openDialog("/registration/twitter", "_blank", {}, function(win) {
+        session.$fetch();
+      });
+    }
+
+    $scope.connectFacebook = function() {
+      openDialog("/registration/facebook", "_blank", {}, function(win) {
+        session.$fetch();
+      });
+    }
+
     // Expose the user in the scope
     $scope.user = session.user;
 
