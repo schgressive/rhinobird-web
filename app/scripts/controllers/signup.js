da1681a923e253b25c7849009305c7dc8d1e92e9
Emilio Blanco <emilioeduardob@gmail.com>
2014-10-27 16:57:37 -0300

    chore(signup): Adds isMobile check

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,32 +1,46 @@
 'use strict';
 
 angular.module('rhinobird.controllers')
-  .controller('SignupCtrl', function ($scope, AuthService) {
-    $scope.registerUser = function(){
+  .controller('SignupCtrl', function ($scope, AuthService, mobileDetect, $timeout) {
+    var vm = this;
 
-      var userData = {
-        user: $scope.regUser
-      };
+    var isMobile = mobileDetect();
+    vm.isMobile = isMobile;
+    vm.signupSuccess = false;
 
-      AuthService.register(userData.user).then(function(e){
-        $scope.$close();
-        var user = e.user;
+    vm.registerUser = registerUser;
+
+    function registerUser(signupForm) {
+      var userData = {};
+      angular.copy(vm.user, userData);
+      userData.isMobile = vm.isMobile;
+
+      AuthService.register(userData).then(function(user){
+        vm.signupSuccess = true;
         AuthService.user.email = user.email;
         AuthService.user.name = user.name;
+
+        // close form after 2 seconds
+        $timeout(function() {
+          $scope.$close();
+        }, 2000);
       },
       function(e){
-        $scope.unknownError = false;
+        vm.unknownError = false;
         if(angular.isObject(e.$response.data) && e.$response.data){
           var info = e.$response.data;
 
           //mark fields as valid = false if there's a need
-          $scope.signupForm.email.$setValidity('taken', (angular.isUndefined(info.email) || info.email[0].match(/taken/) === null));
-          $scope.signupForm.email.$setValidity('invalid', (angular.isUndefined(info.email) || info.email[0].match(/invalid/) === null));
-          $scope.signupForm.password.$setValidity('minlength', (angular.isUndefined(info.password) || info.password[0].match(/short/) === null));
-          $scope.signupForm.username.$setValidity('taken', (angular.isUndefined(info.username) || info.username[0].match(/taken/) === null));
+          signupForm.email.$setValidity('taken', (angular.isUndefined(info.email) || info.email[0].match(/taken/) === null));
+          signupForm.email.$setValidity('invalid', (angular.isUndefined(info.email) || info.email[0].match(/invalid/) === null));
+          if (!angular.isUndefined(signupForm.password)) {
+            signupForm.password.$setValidity('minlength', (angular.isUndefined(info.password) || info.password[0].match(/short/) === null));
+            signupForm.password.$setValidity('match', (angular.isUndefined(info.password) || info.password[0].match(/match/) === null));
+            signupForm.username.$setValidity('taken', (angular.isUndefined(info.username) || info.username[0].match(/taken/) === null));
+          }
         }
         else{
-          $scope.unknownError = true;
+          vm.unknownError = true;
         }
       });
     };
