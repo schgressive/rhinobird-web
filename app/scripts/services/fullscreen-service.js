6f8355f97209fe2f2e0408f6d62c5a458b8abb74
Alvaro Gil <zevarito@gmail.com>
2014-12-11 18:14:34 -0200

    add fullscreen action while streaming
    finishes [#83903832]
    
    * Uses https://github.com/sindresorhus/screenfull.js

---
+++ app/scripts/services/fullscreen-service.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* global google: false */
+
+angular.module('rhinobird.services').
+  service('FullscreenService', function($document, $rootScope) {
+
+    var API = {
+      isEnabled: function () {
+        return (screenfull.enabled && screenfull.isFullscreen);
+      },
+
+      toggleFullscreen: function (element) {
+        if (!screenfull.enabled)
+          return;
+
+        if(this.isEnabled()) {
+          screenfull.exit();
+        } else if (element) {
+          screenfull.request(element);
+        } else {
+          screenfull.request();
+        }
+      }
+    }
+
+    $document.on(screenfull.raw.fullscreenchange, function () {
+      var enabled = API.isEnabled();
+      $rootScope.isFullScreenEnabled = enabled;
+      $rootScope.$emit('fullscreen-change', enabled);
+      $rootScope.$digest();
+    });
+
+    return API;
+});
+
