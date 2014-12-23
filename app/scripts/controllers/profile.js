c3cee31fe42935a79447f77611aeb31fb6f42324
Emilio Blanco <emilioeduardob@gmail.com>
2014-12-23 13:10:29 -0300

    chore(Profile): Change to ControllerAs syntax

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,26 +1,28 @@
 'use strict';
 
 angular.module('rhinobird.controllers')
-  .controller('ProfileCtrl', function ($scope, User, session, $location, OpenAndWatch) {
+  .controller('ProfileCtrl', function (User, session, $location, OpenAndWatch) {
 
-    $scope.self = $scope;
-
-    $scope.connectPopup = function(network) {
-      OpenAndWatch.open("/registration/" + network + "?popup=true", "_blank", {}, function(win) {
-        session.$fetch();
-      });
-    }
+    var vm = this;
 
     // Expose the user in the scope
-    $scope.user = session.user;
-
+    vm.user = session.user;
     // Expose the user streams
     var userTmp = User.$build({username: session.user.username});
-    $scope.timeline = userTmp.timeline.$collection({ pending: true });
-    $scope.timeline.getNextPage();
+    vm.timeline = userTmp.timeline.$collection({ pending: true });
+    vm.timeline.getNextPage();
 
+    // Expose methods to the VM
+    vm.getClass = getClass;
+    vm.connectPopup = connectPopup;
+    vm.disconnect = disconnect;
+    vm.updateSettings = updateSettings;
+
+
+    // PRIVATE METHODS
+    //
     // apply active class to tabs
-    $scope.getClass = function(path) {
+    function getClass(path) {
       if ($location.path().substr(0, path.length) === path) {
         return 'active';
       } else {
@@ -28,13 +30,19 @@ angular.module('rhinobird.controllers')
       }
     };
 
-    $scope.disconnect = function(network) {
-      $scope.user[network + "_token"] = null;
-      $scope.updateSettings();
+    function connectPopup(network) {
+      OpenAndWatch.open("/registration/" + network + "?popup=true", "_blank", {}, function(win) {
+        session.$fetch();
+      });
     }
 
-    $scope.updateSettings = function(){
-      $scope.user.$save();
+    function disconnect(network) {
+      vm.user[network + "_token"] = null;
+      vm.updateSettings();
+    }
+
+    function updateSettings(){
+      vm.user.$save();
     };
 
   });
