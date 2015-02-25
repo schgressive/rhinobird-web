6a100df81ff616bfdf2a6640e334bc6121e4bca7
Alvaro Gil <zevarito@gmail.com>
2015-02-25 11:35:13 -0200

    add hide intro video when not in main state

--- app/scripts/controllers/default_actions.js
+++ app/scripts/controllers/default_actions.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('rhinobird.controllers')
-  .controller('DefaultActionsCtrl', function ($scope, AuthService) {
+  .controller('DefaultActionsCtrl', function ($scope, AuthService, $state) {
     $scope.user = AuthService.user;
+
+    $scope.showVideo = $state.includes('main');
 
     $scope.login = function(){
       AuthService.askLogin();
