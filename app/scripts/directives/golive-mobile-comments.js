07d4edf242a43a21bb11fb3c72d9e0212121f9ae
Alvaro Gil <zevarito@gmail.com>
2015-03-24 14:05:53 -0300

    scroll golive comments to the latest comment

--- app/scripts/directives/golive-mobile-comments.js
+++ app/scripts/directives/golive-mobile-comments.js
@@ -16,15 +16,20 @@ angular.module('rhinobird.directives')
 
       // HTML Elements
       //
-      var loadMoreLink = element.find('.rb-load-more'),
-          submitForm   = element.find('.golive-feed form'),
-          textInput    = element.find('.golive-feed .comment-input');
+      var loadMoreLink      = element.find('.rb-load-more'),
+          submitForm        = element.find('.golive-feed form'),
+          textInput         = element.find('.golive-feed .comment-input'),
+          commentsContainer = element.find('.feed-list');
 
       // Helpers
       //
       function prepareMessageForDisplay(message) {
         message.user = User.$buildRaw(message.user);
         return message;
+      }
+
+      function scrollToBottom() {
+        commentsContainer.scrollTop(commentsContainer.prop("scrollHeight"));
       }
 
       // API Bindings
@@ -41,15 +46,15 @@ angular.module('rhinobird.directives')
       }, this);
 
       CommentsService.API.on('incomming-message', function (message) {
-        scope.comments.unshift(prepareMessageForDisplay(message));
+        scope.comments.push(prepareMessageForDisplay(message));
 
         if (scope.user.username == message.user.username) {
           scope.currentCommentIndex = scope.comments.indexOf(message);
         } else {
           scope.currentCommentIndex++;
         }
-
         scope.$apply();
+        scrollToBottom();
       }, this);
 
       // HTML Bindings
